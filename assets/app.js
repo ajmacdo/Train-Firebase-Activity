59ecd34ba80b803c86f4bd79e7eae5dd4efc866e
aj mac <macdonalda47@gmail.com>
2020-06-14 16:01:57 -0500

    troubleshooting with firebase: new fb app, db, keys, links

--- assets/app.js
+++ assets/app.js
@@ -1,92 +1,112 @@
-var config = {
-apiKey: "AIzaSyDrzIYMhO4lX2JXNDrni4O1i6D3jNypck4",
-authDomain: "train-playing.firebaseapp.com",
-databaseURL: "https://train-playing.firebaseio.com",
-projectId: "train-playing",
-storageBucket: "train-playing.appspot.com",
+var firebaseConfig = {
+    //new fbase credentials generated again 6.14:
+    apiKey: "AIzaSyDE5LGrD3kmrPujELG6OoamqhBOhepeW2w",
+    authDomain: "revisiting-train-asgt.firebaseapp.com",
+    databaseURL: "https://revisiting-train-asgt.firebaseio.com",
+    projectId: "revisiting-train-asgt",
+    storageBucket: "revisiting-train-asgt.appspot.com"
+
+    // apiKey: "AIzaSyDvnD4hXZamWDdxop24f2om_S8H8pXc6PE",
+    // authDomain: "train-asgt.firebaseapp.com",
+    // databaseURL: "https://train-asgt.firebaseio.com",
+    // projectId: "train-asgt",
+    // storageBucket: "train-asgt.appspot.com",
+    // messagingSenderId: "57015589792",
+    // appId: "1:57015589792:web:a7ad2a0939ed148c0be2c5",
+    // measurementId: "G-N0CM2LWCYH"
 };
 
-firebase.initializeApp(config);
 
-// firebase.database().ref().on()
 
-// // var config = {
-// //     apiKey: "AIzaSyB4Ws5gPo9gNW9x90uXnX6XZ4uqE5QjkUY",
-// //     authDomain: "countdownclicker.firebaseapp.com",
-// //     databaseURL: "https://countdownclicker.firebaseio.com",
-// //     storageBucket: "countdownclicker.appspot.com",
-// //     messagingSenderId: "435604262542"
-// //   };
-  
-//   firebase.initializeApp(config);
-  
-  // Create a variable to reference the database
-  var database = firebase.database();
-  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+// Create a variable to reference the database
+var database = firebase.database().ref();
+
+var trainName;
+var destination;
+var firstTime;
+var frequency = 0;
+
 //create button to add train
 $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
 
-//create vars to store user inputs  
-var name = $("#train-name-input").val().trim();
-  var dest = $("#destination-input").val().trim();
-  var firstTime = moment($("#first-time-input").val().trim(), "__:__ military time").format("HH:mm"); //format('HHmm')
-  var freq = $("#frequency-input").val().trim();
+    //create vars to store user inputs  
+    trainName = $("#train-name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTime = moment($("#first-time-input").val().trim(), "__:__ military time").format("HH:mm"); //format('HHmm')
+    frequency = $("#frequency-input").val().trim();
 
-//create object to hold added train data
-var newTrain = {
-    trainName: name,
-    destination: dest,
-    firstTime: firstTime,
-    frequency: freq
-};
+    //create object to hold added train data
+    var newTrain = {
+        trainName: trainName,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
 
-//transfer new train info/object to database
-database.ref().push(newTrain);
+    //transfer new train info/object to database
+    database.ref().push(newTrain);
 
-//log to console
-console.log(newTrain.trainName);
-console.log(newTrain.destination);
-console.log(newTrain.firstTime);
-console.log(newTrain.frequency);
+    //log to console
+    console.log(newTrain.trainName);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTime);
+    console.log(newTrain.frequency);
 
-alert("Train successfully added");
+    alert("Train successfully added");
 
-//clear input fields
-$("#train-name-input").val("");
-$("#destination-input").val("");
-$("#first-time-input").val("");
-$("#frequency-input").val("");
+    //clear input fields
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-time-input").val("");
+    $("#frequency-input").val("");
 });
 
-//translate user inputs to new row after making this id'd as a firebase event that is a child added to the firebase snapshot
+//HERE where there's issue? Firebase not working?  all above seems to be functioning. //translate user inputs to new row after making this id'd as a firebase event that is a child added to the firebase snapshot
 database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
 
+    //store in variables...
+    var name = childSnapshot.val().trainName;
+    var destination = childSnapshot.val().destination;
+    var firstTime = childSnapshot.val().firstTime;
+    var frequency = childSnapshot.val().frequency;
 
-//store in variables...
-  var name = childSnapshot.val().trainName;
-  var dest = childSnapshot.val().destination;
-  var firstTime = childSnapshot.val().firstTime;
-  var freq = childSnapshot.val().frequency;
+    // train info
+    console.log(name);
+    console.log(destination);
+    console.log(firstTime);
+    console.log(frequency);
 
 
-  // train info
-  console.log(name);
-  console.log(dest);
-  console.log(firstTime);
-  console.log(freq);
+    //have to write code here (?) for calculating nextArrival and minutesAway based on the first train time & frequency of that train.
 
-//create new row
-var newRow = $("<tr>").append(
-    $("<td>").text(name),
-    $("<td>").text(dest),
-    $("<td>").text(freq), 
-    $("<td>").text(nextArrival),
-    $("<td>").text(minutesAway)
-);
+    //var minutesAway = moment().diff(moment(firstTime, "X"), "minutes");
+    //console.log(minutesAway); //??need to look into moment doc'tn
 
-//make new row append to table
-$("#train-table > tbody").append(newRow);
+    //need to take time now, and display the next arrival from that
+
+    // var minutesAway = moment() - next
+
+    // var nextArrival = 
+
+
+    //create new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minutesAway)
+    );
+
+    //make new row append to table
+    $("#train-table > tbody").append(newRow);
 
 });
